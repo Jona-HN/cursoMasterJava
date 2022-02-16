cf4892e24dc4d7f7ee0db98d706bd5140cdf0020
Jonathan Herrera <jherrera@certuit.com>
2022-02-16 10:24:20 -0800

    Tarea 2

---
+++ HelloWorld/src/DetalleDeFactura.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class DetalleDeFactura
+{
+    private String nombre;
+    private double precio1;
+    private double precio2;
+    private double subtotal;
+    private double impuesto;
+    private double total;
+
+    public DetalleDeFactura(){}
+
+    @Override
+    public String toString()
+    {
+        return "La factura " + nombre + " tiene un total bruto de " + subtotal +
+                ", con un impuesto de " + impuesto + " y el monto después de impuesto es de " +
+                total;
+    }
+
+    public static void main(String[] args)
+    {
+       Scanner sc = new Scanner(System.in);
+       DetalleDeFactura factura = new DetalleDeFactura();
+       System.out.println("Ingrese el nombre de la factura: ");
+       factura.nombre = sc.nextLine();
+       System.out.println("Ingrese el primer precio: ");
+       factura.precio1 = sc.nextDouble();
+       System.out.println("Ingrese el segundo precio: ");
+       factura.precio2 = sc.nextDouble();
+       factura.subtotal = factura.precio1 + factura.precio2;
+       factura.impuesto = factura.subtotal * 0.19;
+       factura.total = factura.subtotal + factura.impuesto;
+       System.out.println(factura.toString());
+    }
+}
