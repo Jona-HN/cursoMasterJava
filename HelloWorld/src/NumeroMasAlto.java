21260467c976025c8ede4de4123f71947be212b9
Jonathan Herrera <jherrera@certuit.com>
2022-02-21 21:32:12 -0800

    Tarea 14

---
+++ HelloWorld/src/NumeroMasAlto.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class NumeroMasAlto
+{
+	public static void main(String[] args)
+	{
+		Random r = new Random();
+		int numeros[] = new int[7];
+
+		for (int i = 0; i < 7; i++)
+		{
+			numeros[i] = r.nextInt(11, 100);
+			System.out.print(numeros[i] + " ");
+		}
+
+		int num_mayor = 0;
+
+		for (int i : numeros)
+		{
+			if(i > num_mayor)
+			{
+				num_mayor = i;
+			}
+		}
+
+		System.out.println();
+		System.out.println("El número mayor es: " + num_mayor);
+	}
+}
