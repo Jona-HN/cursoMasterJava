0db135e5983aedf90dd6abd47202434ea1085e9b
Jonathan Herrera <jherrera@certuit.com>
2022-02-21 21:08:57 -0800

    Tarea 13

---
+++ HelloWorld/src/OrdenArreglos.java
@@ -0,0 +1,18 @@
+public class OrdenArreglos
+{
+	public static void main(String[] args)
+	{
+		int numeros[] = new int[10];
+
+		for (int i = 0; i < 10; i++)
+		{
+			numeros[i] = Integer.parseInt(args[i]);
+		}
+
+		for (int i = 0; i < numeros.length / 2; i++)
+		{
+			System.out.print(numeros[numeros.length - (i + 1)]);
+			System.out.print(" " + numeros[i] + " ");
+		}
+	}
+}
