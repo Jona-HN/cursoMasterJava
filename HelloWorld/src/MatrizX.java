089c391e8654d0902854aa072e364e170c9079a7
Jonathan Herrera <jherrera@certuit.com>
2022-02-23 10:43:32 -0800

    Tarea 18

---
+++ HelloWorld/src/MatrizX.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+public class MatrizX
+{
+	public static void main(String[] args)
+	{
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la dimensión de la matriz: "));
+
+		if (n < 1)
+		{
+			System.err.println("Se ha ingresado un valor menor a 1");
+			System.exit(-1);
+		}
+
+		char[][] matriz = new char [n][n];
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (i == j || i + j == n - 1)
+				{
+					matriz[i][j] = 'X';
+				}
+				else
+				{
+					matriz[i][j] = '_';
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				System.out.print(matriz[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
